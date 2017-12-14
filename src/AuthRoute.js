d470db188a20df0802df9481355d80026f26d604
roy ledford <bluesixty@gmail.com>
2017-12-14 10:11:45 -0500

    Delete old files - add axios - update readme.md

--- src/AuthRoute.js
+++ src/AuthRoute.js
@@ -1,73 +1,39 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Redirect, Route } from 'react-router-dom';
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Redirect, Route } from 'react-router-dom'
 
 //Mock of an Auth method, can be replaced with an async call to the backend. Must return true or false
-const isAuthenticated = () => true;
+const isAuthenticated = () => true
 
-const PRIVATE_ROOT = '/private';
-const PUBLIC_ROOT = '/login';
+const PRIVATE_ROOT = '/private'
+const PUBLIC_ROOT = '/login'
 
-const AuthRoute = ({component, ...props}) => {
-  const { isPrivate } = component;
+const AuthRoute = ({ component, ...props }) => {
+  const { isPrivate } = component
 
   if (isAuthenticated()) {
     //User is Authenticated
     if (isPrivate === true) {
       //If the route is private the user may proceed.
-      return <Route { ...props } component={ component } />;
-    }
-    else {
+      return <Route {...props} component={component} />
+    } else {
       //If the route is public, the user is redirected to the app's private root.
-      return <Redirect to={ PRIVATE_ROOT } />;
+      return <Redirect to={PRIVATE_ROOT} />
     }
-  }
-  else {
+  } else {
     //User is not Authenticated
     if (isPrivate === true) {
       //If the route is private the user is redirected to the app's public root.
-      return <Redirect to={ PUBLIC_ROOT } />;
-    }
-    else {
+      return <Redirect to={PUBLIC_ROOT} />
+    } else {
       //If the route is public, the user may proceed.
-      return <Route { ...props } component={ component } />;
+      return <Route {...props} component={component} />
     }
   }
-};
+}
 
 AuthRoute.propTypes = {
-  component: PropTypes.oneOfType([
-    PropTypes.element,
-    PropTypes.func
-  ])
-};
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func]),
+}
 
-export default AuthRoute;
-
-
-// const AuthRoute = ({component, ...props}) => {
-//   const { isPrivate } = component;
-
-//   if (isAuthenticated()) {
-//     //User is Authenticated
-//     if (isPrivate === true) {
-//       //If the route is private the user may proceed.
-//       return <Route { ...props } component={ component } />;
-//     }
-//     else {
-//       //If the route is public, the user is redirected to the app's private root.
-//       return <Redirect to={ PRIVATE_ROOT } />;
-//     }
-//   }
-//   else {
-//     //User is not Authenticated
-//     if (isPrivate === true) {
-//       //If the route is private the user is redirected to the app's public root.
-//       return <Redirect to={ PUBLIC_ROOT } />;
-//     }
-//     else {
-//       //If the route is public, the user may proceed.
-//       return <Route { ...props } component={ component } />;
-//     }
-//   }
-// };
+export default AuthRoute
--- src/components/Header/HeaderTitle.js
+++
@@ -1,8 +0,0 @@
-import React, { Component } from 'react'
-import './HeaderTitle.css'
-
-export default class HeaderTitle extends Component {
-  render() {
-    return <p className="headertitle">Merchant</p>
-  }
-}
